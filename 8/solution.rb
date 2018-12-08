388e149ff7b1bdd1a0a20abfa249eecb87e7bf87
meshiest <meek.mesh@gmail.com>
2018-12-08 16:45:06 -0500

    Sum is an iterator!

--- 8/solution.rb
+++ 8/solution.rb
@@ -14,18 +14,18 @@ end
 
 # Sum up the metas of the node and its children
 def meta_sum node
-  node[:nodes].map{ |n| meta_sum n }.sum + node[:meta].sum
+  node[:nodes].sum{ |n| meta_sum n } + node[:meta].sum
 end
 
 # Get the node value
 def node_value node
   node[:nodes].empty? ?
     node[:meta].sum : # Value is sum of meta if there are no children
-    node[:meta].map{ |i|
+    node[:meta].sum{ |i|
       node[:nodes][i - 1] ? # Value is value of Ith child or 0
         (node_value node[:nodes][i - 1]) :
         0
-    }.sum
+    }
 end
 
 # Get build the tree
